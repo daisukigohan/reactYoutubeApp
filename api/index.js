8a2490d8a8af59dde0982cebfb954926781cc9f2
Ponta <bayashiko.daiki@gmail.com>
2021-02-03 23:45:44 +0900

    Add favorite button api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { google } = require('googleapis');
+const { readFavoriteIds, writeFavoriteIds } = require('../utils/favorite');
 
 // 環境変数からYOUTUBE_API_KEYを取得
 // 実行時には環境変数YOUTUBE_API_KEYに取得したAPIキーを設定して実行する
@@ -34,7 +35,25 @@ router.get('/videos/search/:keyword', (req, res, next) => {
   })().catch(next);
 });
 
-// 以下追加する
+// お気に入り動画の取得
+router.get('/videos/favorites', (req, res, next) => {
+  (async () => {
+    // お気に入り動画IDを取得
+    const favoriteIds = await readFavoriteIds();
+    if (!favoriteIds.length) {
+      // お気に入りが1つもなければ空配列を返す
+      res.json({ items: [] });
+      return;
+    }
+    // お気に入りのIDから動画の取得
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: favoriteIds.join(','),
+    });
+    res.json({ items });
+  })().catch(next);
+});
+
 // 動画詳細情報取得
 router.get('/videos/:videoId', (req, res, next) => {
   const { videoId } = req.params;
@@ -70,5 +89,48 @@ router.get('/videos/:videoId/related', (req, res, next) => {
     res.json({ items, nextPageToken });
   })().catch(next);
 });
+
+// お気に入り動画ID一覧取得
+router.get('/favorites', (req, res, next) => {
+  readFavoriteIds().then((data) => {
+    res.json(data);
+  }).catch(next);
+});
+
+// お気に入り登録・解除
+router.route('/favorites/:id')
+  // お気に入り登録
+  .post((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      if (favoriteIds.indexOf(id) === -1) {
+        // パラメータに指定されたIDが現時点のお気に入りになかった場合
+        // お気に入りリストに追加
+        favoriteIds.unshift(id);
+        // お気に入りリストを書き込む
+        writeFavoriteIds(favoriteIds);
+      }
+      res.end();
+    })().catch(next);
+  })
+
+  // お気に入り解除
+  .delete((req, res, next) => {
+    (async () => {
+      const { id } = req.params;
+      // 現時点のお気に入りリストを読み込み
+      const favoriteIds = await readFavoriteIds();
+      const indexOfId = favoriteIds.indexOf(id);
+      if (indexOfId !== -1) {
+        // パラメータに指定されたIDが現時点のお気に入りにあった場合
+        // 指定されたIDを削除したものをお気に入りリストに書き込む
+        writeFavoriteIds(favoriteIds.filter((favoriteId) => (favoriteId !== id)));
+      }
+      res.end();
+    })().catch(next);
+  });
+
 
 module.exports = router;
